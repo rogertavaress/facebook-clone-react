8d9e6c714767e7de5d11cdfe270391c637ee3e43
Rogerio Tavares <rogerioctf@gmail.com>
2020-03-09 09:02:46 -0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 
 import Header from "./components/header/header";
 import Body from "./components/body/body";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <>
       <Header />
       <Body />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+class footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Feito com React por Rogério Tavares</p>
+      </footer>
+    );
+  }
+}
+
+export default footer;
